b02243e0addcd9fc11ff34bc1f54dde8f8a71092
akshar <akshargothi70@gmail.com>
2025-09-19 11:14:50 -0700

    Live URL

--- front-end-app/src/apis/api.js
+++ front-end-app/src/apis/api.js
@@ -3,7 +3,7 @@ let API_URL;
 if (process.env.REACT_APP_ENVIRONMENT === "development") {
   API_URL = "http://localhost:3003";
 } else {
-  API_URL = ""; // or whatever your production API URL is
+  API_URL = "http://18.218.238.186/"; // or whatever your production API URL is
 }
 
 export default API_URL;
